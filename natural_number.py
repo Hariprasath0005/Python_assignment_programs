6aabcc5cc60d81d3c0fd149afd7aafae8bc30e1b
Hariprasath0005 <t.hariprasath5301@gmail.com>
2024-02-26 23:46:47 +0530

    number is positive, negative or zero

---
+++ natural_number.py
@@ -0,0 +1,10 @@
+#Program to check if a number is positive, negative or zero
+
+num = int(input("Enter the Number: "))
+if num>=1:
+    print("Positive Number")
+elif num<=1:
+    print("Negative Number")
+else:
+    print("Zero")
+    
