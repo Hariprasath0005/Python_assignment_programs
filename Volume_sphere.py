9659985d10cc086520bdd23610d181b361e3d8e9
Hariprasath0005 <t.hariprasath5301@gmail.com>
2024-02-27 00:05:48 +0530

    volume of a sphere

---
+++ Volume_sphere.py
@@ -0,0 +1,6 @@
+#Program to get the volume of a sphere V=4 / 3 πr3
+
+pi = 3.14
+radius = float(input(" Enter the Radius of sphere: "))
+vol = (4/3)*3.14*(radius**3)
+print("The volume of the sphere is :", vol)
