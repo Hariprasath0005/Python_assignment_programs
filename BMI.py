9ff46e2ba5ddbf7e3fc32466761433bf0d1fd782
Hariprasath0005 <t.hariprasath5301@gmail.com>
2024-02-26 23:25:56 +0530

    BMI

---
+++ BMI.py
@@ -0,0 +1,6 @@
+#Program to calculate body mass index
+
+weight = float(input("Enter the weight in kg: "))
+height = float(input("Enter the height in m: "))
+bmi = weight /height**2
+print("BMI :",bmi)
