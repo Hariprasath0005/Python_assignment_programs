34ea7693fb887632e6b81280ce74b1c5a82b1a1f
Hariprasath0005 <t.hariprasath5301@gmail.com>
2024-02-26 23:23:49 +0530

    Area of Triangle

---
+++ areaoftriangle.py
@@ -0,0 +1,5 @@
+#Program to find area of a triangle
+base = float(input("Enter the base value: "))
+height = float(input("Enter the heigth value: "))
+area  = 1/2 * base * height
+print("Area: ",area)
