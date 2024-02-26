52f39d93629f72aad4e87f35eb80c99e9a1e01cd
Hariprasath0005 <t.hariprasath5301@gmail.com>
2024-02-27 00:06:38 +0530

    letter is a vowel or not

---
+++ Vowel_or_not.py
@@ -0,0 +1,8 @@
+#Program to test whether a passed letter is a vowel or not
+
+str = input("Enter a letter to check vowel: ")
+if str == "a" or str =="e" or str =="i" or str =="o" or str =="u" or str =="A" or str =="E" or str =="I" or str =="O" or str =="U":
+    print("Is Vowel")
+else:
+    print("Not a Vowel")
+    
